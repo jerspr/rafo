60c7f7363194744e068cc17ae7ce4ba510cd97a8
jesser pariona <jerspr@gmail.com>
2016-05-08 18:40:33 -0500

    üpdate

---
+++ index2.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="title" content="Rafo Ra&eacute;z" />
+<meta name="description" content="Web Oficial de Rafo Ra&eacute;z" />
+<meta name="keywords" content="Rafo Ra&eacute;z" />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="Rafo Ra&eacute;z" />
+<meta name="author url" content="http://raforaez.com/" />
+<meta name="base" content="http://raforaez.com/" />
+<meta name="google-site-verification" content="ZtXi3VP5xPxXp0PffBhaeosOjSvCqZakG1Wt0sajEJU" />
+<title>Diversidad de ser peruano | Rafo Ráez</title>
+<link href="css/reset.css" type="text/css" rel="stylesheet" media="all" />
+<link href="css/global.css" type="text/css" rel="stylesheet" media="all" />
+<link href="fancybox/jquery.fancybox-1.3.1.css" type="text/css" rel="stylesheet" media="all" />
+<!--[if lte IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="screen"/><![endif]-->
+<!--[if IE 7]><link rel="stylesheet" href="css/ie7.css" type="text/css" media="screen"/><![endif]-->
+
+
+</head>
+<body id="inicio">
+<div id="cabecera">
+  <div id="bg-menu-social">
+    <div id="menu-social">
+      <div id="player-flash"></div>
+      <ul id="menu-principal">
+        <li class="inicio"><a class="activo" href="index.html"><span>Inicio</span></a></li>
+        <li class="discografia"><a href="discografia.html"><span>Discografía</span></a></li>
+        <li class="claves"><a href="#"><span>Claves del artista</span></a></li>
+        <li class="shows"><a href="shows.html"><span>Show</span></a></li>
+        <li class="bonus"><a class="jbonus" href="bonus.html"><span>Bonus</span></a></li>
+        <li class="contacto"><a href="#"><span>Contacto</span></a></li>
+      </ul>
+      <a href="http://www.youtube.com/reparanoico" id="ico-youtube" target="_blank" title="Canal Youtube"></a> <a href="http://www.myspace.com/raforaez" id="ico-mysapce" target="_blank" title="Canal MySpace"></a> <a href="http://www.facebook.com/pages/Rafo-Raez/113738985310326?ref=ts" id="ico-facebook" target="_blank" title="Canal Facebook"></a> <a href="http://www.flickr.com/search/show/?q=rafo+raez" id="ico-flickr" target="_blank" title="Canal Flickr"></a><!-- <a href="#" id="ico-espana" title="Version Espanol"></a> <a href="#" id="ico-inglaterra" title="Version Ingles"></a>--> </div>
+	<div id="recomendar"><iframe id="iframe-like" src="http://www.facebook.com/plugins/like.php?href=http%253A%252F%252Fraforaez.com%252F&amp;layout=button_count&amp;show_faces=false&amp;width=115&amp;action=recommend&amp;font&amp;colorscheme=light&amp;height=21" 
+    scrolling="no" frameborder="0" allowTransparency="true"></iframe></div>	
+  </div>
+  <div id="logo-banner">
+    <h1 id="logo"><a href="index.html"><span>Rafo Raez</span></a></h1>
+    <div class="banner-660x90" id="banner-660x90"></div>
+  </div>
+</div>
+<div id="contenido-inicio"> 
+	<a id="enlace-claves" href="#"><span>Claves del artista</span></a> 
+    <a id="enlace-discografia" href="discografia.html"><span>Discografía</span></a> 
+    <a id="enlace-bonus"  class="jbonus" href="bonus.html"><span>Bonus</span></a> 
+    <a id="enlace-show" href="shows.html"><span>Show</span></a>
+    <a id="ancla-claves" name="ancla-claves">Ancla para claves del artista</a>
+  <div id="player-index"></div>
+  <h2 id="titulo-claves"><span>Claves de artista</span></h2>
+  <h2 id="titulo-noticias"><span>Noticias</span></h2>
+</div>
+<div id="cnt-texto">
+  <div id="claves-texto">
+    <ul>
+      <li> <span class="contador">0</span> <span class="clave-detalle"> Me gusta escuchar discos que muestran distintos sentimientos y técnicas, creo que el disco ofrece más al oyente si le ofrece un vecindario donde él pueda volar o bucear hacia adonde quiera.
+        El catálogo esta bien para clasificar la música por espacios (discotecas, estadios, festivales temáticos, etc) pero los CDs míos no están hechos según la clasificación del catálogo. </span> </li>
+      <li> <span class="contador">1</span> <span class="clave-detalle"> Soy un servidor público, en la performance reorganizo mi material de acuerdo al espacio y quien nos convoca. Considero que todos somos algo como 50% y 50%, comunes e individuales, ordinarios y especiales, etc. Mi discografía es mi comunicación más individual, los shows son una fiesta social, un compartir. </span> </li>
+      <li> <span class="contador">2</span> <span class="clave-detalle"> Este es un trabajo de equipo, los individuos somos un trabajo de equipo, empezando por nuestra Patria y lo mucho que ella nos define. Yo canto en el idioma del Perú, tocó cualquier instrumento con el idioma del Perú. </span> </li>
+      <li> <span class="contador">3</span> <span class="clave-detalle"> A menudo pienso en La Muerte, te da una perspectiva de La Vida. La Vida me gusta, estoy seguro de que La Muerte también me va a gustar. </span> </li>
+    </ul>
+    <span id="cnt-bio"><a id="bio" title="Biografia" href="biografia.html"></a></span> </div>
+  <div id="noticias-texto">
+    <?=file_get_contents("notas.html")?>
+    <p class="enlace-noticias">Gracias a: <a href="http://www.raforaez.blogspot.com" target="_blank" title="Rafo Raez Blogspot">http://www.raforaez.blogspot.com</a> </p>
+    <a href="http://www.raforaezperu.tk" target="_blank" id="unete-komuna"></a> </div>
+</div>
+<div id="footer">
+  <p>Copyright © 2010 Rafo Ráez.* Todos los derechos reservados. Agradecimientos: Musuk Nolte, Jorge Pérez, Carlitos Raymundo, Chicho, Luigi, Harold, Joan Peramás, Angie Saravia</p>
+  <a id="unipersonal-f" href="#" title="Unipersonal Rafo raez"></a> <a id="paranoias" href="#" title="Rafo Raez y los Paranoias"></a> <a id="raforaez" href="#" title="Rafo Raez"></a> <a id="komuna" href="#" title="Komuna Rafo Raez"></a> <a id="blogspot" href="#" title="Rafo Raez Blogspot"></a> <a id="tuwebperu" href="#" title="tuWebPeru.Net"></a> </div>
+<div id="footer-down">
+<ul>
+<li><a href="index.php" class="activo">Inicio</a></li>
+<li><a href="discografia.html">Discografía</a></li>
+<li><a id="claves-bajo" href="#">Claves del Artista</a></li>
+<li><a href="shows.html">Shows</a></li>
+<li><a class="jbonus" href="bonus.html">Bonus</a></li>
+</ul>
+</div>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel-3.0.2.pack.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox-1.3.1.pack.js"></script>
+<script type="text/javascript" src="js/jquery-swfobject.js"></script>
+<script type="text/javascript">  
+$('#player-index').flash({
+swf: 'swf/botones_index.swf',
+wmode: 'transparent',
+width: 310,
+height: 297
+});
+$('#banner-660x90').flash({   
+swf: 'swf/banner003.swf',
+width: 660,
+height: 90    
+ }); 
+$('#player-flash').flash({   
+swf: 'mp3player.swf',
+width: 188,
+height: 39    
+ });  
+ $(document).ready(function() {
+    $("#bio").fancybox({
+        'padding'	: 0,
+        'autoScale'	: false,
+        'width'         : 800,
+        'height'        : 720,
+        'easingIn'      : 'easeOutBack',
+        'easingOut'     : 'easeInBack',
+        'type'				: 'iframe'
+    });
+    $(".jbonus").fancybox({
+        'padding'       : 0,
+        'autoScale'	: false,
+        'easingIn'      : 'easeOutBack',
+        'easingOut'     : 'easeInBack'
+    });
+});
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16517705-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+</body>
+</html>
